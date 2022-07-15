473f9f898910e5e5927715b583ea7b7daede74fa
Sajal Sharma <sajalsharma2408@gmail.com>
2022-07-15 13:56:12 +0530

    working with router

--- node_part/routes/workout.js
+++ node_part/routes/workout.js
@@ -1,2 +1,16 @@
 const express = require("express");
 const router = express.Router();
+
+router.get("/", (req, res) => {
+  res.json("Get all workouts");
+});
+
+router.get("/:id", (req, res) => {
+  res.json("Get a single workout");
+});
+
+router.get("", (req, res) => {});
+
+router.get("", (req, res) => {});
+
+module.exports = router;
